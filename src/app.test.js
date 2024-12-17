5afd05c0f90d72081fc8fe73b3d839677671ac31
Michael M. Werfring <michaelmaximilianw@gmail.com>
2024-12-17 18:15:28 +0100

    Fixing new Testcase

--- src/app.test.js
+++ src/app.test.js
@@ -13,13 +13,13 @@ describe("GET /", () => {
   });
 });
 
-describe("GET /hello", () => {
+describe("GET /production", () => {
   it("should return a 200 status and the correct JSON response", async () => {
-    const response = await request(app).get("/");
+    const response = await request(app).get("/production");
 
     expect(response.status).toBe(200);
     expect(response.body).toEqual({
-      message: "Greetings mister unknown",
+      message: "This is a production route.",
     });
   });
 });
