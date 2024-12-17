cbe0d327b9dd5c9270bb7255230cbfaf3c6544f5
Michael M. Werfring <michaelmaximilianw@gmail.com>
2024-12-17 18:13:00 +0100

    Adding test for hello endpoint

--- src/app.test.js
+++ src/app.test.js
@@ -12,3 +12,14 @@ describe("GET /", () => {
     });
   });
 });
+
+describe("GET /hello", () => {
+  it("should return a 200 status and the correct JSON response", async () => {
+    const response = await request(app).get("/");
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({
+      message: "Greetings mister unknown",
+    });
+  });
+});
